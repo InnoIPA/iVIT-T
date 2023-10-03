e0ab80a4784186ed9fd740536f0f4604e2411fc0
ChangLijie <lichieh_chang@innodisk.com>
2023-10-03 09:43:10 +0800

    feat : stop ivit-t

---
+++ docker/stop_ivit_t.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Copyright (c) 2023 Innodisk Corporation
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+docker stop ivit-t
+docker stop ivit-t-postgres
+docker stop ivit-t-webui
