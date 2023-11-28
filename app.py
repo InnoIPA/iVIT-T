c5409695f6665a878f965fb9af866436e70dcf03
ChangLijie <lichieh_chang@innodisk.com>
2023-11-28 17:44:47 +0800

    feat : add training schedule.

--- app.py
+++ app.py
@@ -3,27 +3,20 @@ from argparse import ArgumentParser, SUPPRESS
 from ivit import config_logger
 from webapi import app, socketio, \
                     app_cl_pj, app_ud_dt, app_dy_dt, app_labeling, app_auto_labeling, \
-                    app_aug, app_cl_model, app_train, app_export, app_eval, app_icap
+                    app_aug, app_cl_model, app_train, app_export, app_eval, app_icap 
 from webapi.common import init_db, init_sample_to_db, init_color_table_db, \
                             init_for_icap, register_mqtt_event, get_target_addr, \
-                            update_version_function, read_json, write_json                                         
+                            update_version_function, read_json, write_json, Schedule
+                                          
 from webapi.common.config import *
-
+from webapi.common.config import SCHEDULE
+import time
 def build_argparser():
     parser = ArgumentParser(add_help=False)
     args = parser.add_argument_group('Options')
     args.add_argument('-h', '--help', action='help', default=SUPPRESS, help='Show this help message and exit.')
     args.add_argument('-port', '--port', required=True, help = "Input port number")
     return parser
-
-def box_web_url(ip,port):
-    web_url = '|    WEBSITE --> http://{}:{}    |'.format(ip, port)
-    leng = len(web_url)
-    logging.info('-'*leng)
-    logging.info('|'+' '*(leng-2)+'|')
-    logging.info(web_url)
-    logging.info('|'+' '*(leng-2)+'|')
-    logging.info('-'*leng)
 
 def main(args):
     # Create initial table in db
@@ -56,9 +49,9 @@ def main(args):
     logging.info('Update HOST to {}'.format(host_ip))
 
     # Register iCAP
-    logging.info("Initial iCAP register...")
-    init_for_icap()
-    register_mqtt_event()    
+    # logging.info("Initial iCAP register...")
+    # init_for_icap()
+    # register_mqtt_event()    
     
     # Setting version.json
     config = read_json(VERSION_PATH)
@@ -67,8 +60,12 @@ def main(args):
     write_json(VERSION_PATH, config)
 
     # Running webapi sever
+    logging.info("Start training schedule...")
+    test_schedule = Schedule()
+    app.config["SCHEDULE"] = test_schedule
+    test_schedule.start()
+ 
     logging.info("Running webapi server...")
-    box_web_url("127.0.0.1",6538)
     socketio.run(app, host = "0.0.0.0", port=args.port, debug=False)
 
 if __name__ == '__main__':
